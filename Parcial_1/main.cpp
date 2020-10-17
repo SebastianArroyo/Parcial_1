2985ee7ab60b320df5f6a67022294c39eb269df7
SebastianArroyo <sebastian.arroyo@hotmail.es>
2020-10-17 18:47:09 -0500

    Primer caso

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     int elec;
-    float altura1=0, altura2=0, dist=0, vel_ini1=0,vel_ini2=0,ang1=0,ang2=0,tiempo=0, tiempo_lim=0;
+    double altura1=0, altura2=0, dist=0, vel_ini1=0,vel_ini2=0,ang1=0,ang2=0,tiempo=0, tiempo_lim=0,g=9.81,tiempo1=0;
     while (true) {
         cout << "Ingrese el punto a ejecutar: "; cin >> elec;
         switch (elec) {
@@ -17,7 +17,26 @@ int main()
             cout << "Ingrese altura del canion defensivo: "; cin >> altura2;
             cout << "Ingrese la distancia entre los caniones: "; cin >> dist;
             tiempo_lim=2.5; //El disparo debe llegar antes de 2.5 s
+            ang1=45;
+            if (altura1>altura2) {
+                double cos,det,det2;
+                cos=sqrt(2)/2;
+                cout<<cos<<endl;
+                for(vel_ini1=0.01;tiempo<2.5;vel_ini1=vel_ini1+0.01) {
+                    det=((vel_ini1*cos)*(vel_ini1*cos))-(4*(g/2)*(altura2-altura1));
+                    det2=vel_ini1*cos+sqrt(det);
+                    tiempo=det2/g;
+                    tiempo1=dist/(cos*vel_ini1);
+                    if(abs(tiempo-tiempo1)<=0.01) {
+                        cout<<"La vel inicial es: "<<vel_ini1<<endl;
+                    }
+                }
+            }
+            if (altura1<altura2) {
+                for (;; ) {
 
+                }
+            }
 
         }
         break;
