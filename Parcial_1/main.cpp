f319eeec6bec17a856b11b8990b266bf425bec1c
SebastianArroyo <sebastian.arroyo@hotmail.es>
2020-10-17 16:02:34 -0500

    Primer commit del codigo Definir variables a usar

---
+++ Parcial_1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int elec;
+    float altura1=0, altura2=0, dist=0, vel_ini1=0,vel_ini2=0,ang1=0,ang2=0;
+    while (true) {
+        cout << "Ingrese el punto a ejecutar: "; cin >> elec;
+        switch (elec) {
+
+        case 1:
+        {
+            cout << "Ingrese altura del canion ofensivo: "; cin >> altura1;
+            cout << "Ingrese altura del canion defensivo: "; cin >> altura2;
+            cout << "Ingrese la distancia entre los caniones: "; cin >> dist;
+
+        }
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+
+        default:
+
+        break;
+
+        }
+    }
+}
