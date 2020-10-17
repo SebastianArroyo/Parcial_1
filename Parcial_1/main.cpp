90018219899669e64251dc77203e5e4c08822b5d
SebastianArroyo <sebastian.arroyo@hotmail.es>
2020-10-17 16:26:34 -0500

    Codigo 1 Para ser un disparo ofensivo exitoso debe impactar en menos de 2.5 segundos

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int elec;
-    float altura1=0, altura2=0, dist=0, vel_ini1=0,vel_ini2=0,ang1=0,ang2=0;
+    float altura1=0, altura2=0, dist=0, vel_ini1=0,vel_ini2=0,ang1=0,ang2=0,tiempo=0, tiempo_lim=0;
     while (true) {
         cout << "Ingrese el punto a ejecutar: "; cin >> elec;
         switch (elec) {
@@ -15,6 +15,8 @@ int main()
             cout << "Ingrese altura del canion ofensivo: "; cin >> altura1;
             cout << "Ingrese altura del canion defensivo: "; cin >> altura2;
             cout << "Ingrese la distancia entre los caniones: "; cin >> dist;
+            tiempo_lim=2.5; //El disparo debe llegar antes de 2.5 s
+
 
         }
         break;
